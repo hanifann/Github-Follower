36e9aa83fdbc03c8b21b665d0309946b2a95d034
hanifan <hanifannurulhaq@outlook.com>
2024-01-02 13:17:26 +0700

    refactor tabbar and navigation controller

--- GithubFollower/SceneDelegate.swift
+++ GithubFollower/SceneDelegate.swift
@@ -16,16 +16,36 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let seachNC = UINavigationController(rootViewController: SearchViewController())
-        let favoriteNC = UINavigationController(rootViewController: FavoriteViewController())
         
-        let tabbar = UITabBarController()
-        tabbar.viewControllers = [seachNC, favoriteNC]
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = tabbar
+        window?.rootViewController = createTabbar()
         window?.makeKeyAndVisible()
+    }
+    
+    func createSearchNavigationController() -> UINavigationController {
+        let searchVC = SearchViewController()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoriteNavigationController() -> UINavigationController {
+        let favoriteVC = FavoriteViewController()
+        favoriteVC.title = "Favorites"
+        favoriteVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoriteVC)
+    }
+    
+    func createTabbar() -> UITabBarController {
+        let tabbar = UITabBarController()
+        UITabBar.appearance().tintColor = .systemGreen
+        tabbar.viewControllers = [createSearchNavigationController(), createFavoriteNavigationController()]
+        
+        return tabbar
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
