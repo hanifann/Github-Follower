095bb0513e2c64b427897f2e30c3a16cd17737ef
hanifan <hanifannurulhaq@outlook.com>
2024-01-02 19:46:02 +0700

    initalize follower view controller

---
+++ GithubFollower/FollowerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FollowerViewController.swift
+//  GithubFollower
+//
+//  Created by hanifan nurul haq on 02/01/24.
+//
+
+import UIKit
+
+class FollowerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
