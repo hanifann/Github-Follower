ebc837ede0c2cc5bf8d2add540313d183470e64b
hanifan <hanifannurulhaq@outlook.com>
2024-01-02 11:35:44 +0700

    Initial Commit

---
+++ GithubFollower/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollower
+//
+//  Created by hanifan nurul haq on 02/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
