57c5bcda384023b0df8361ddd9ef2ded0990605c
hanifan <hanifannurulhaq@outlook.com>
2024-01-02 13:03:59 +0700

    initiate search and favorite view controller

---
+++ GithubFollower/FavoriteViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteViewController.swift
+//  GithubFollower
+//
+//  Created by hanifan nurul haq on 02/01/24.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemMint
+    }
+
+}
---
+++ GithubFollower/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  GithubFollower
+//
+//  Created by hanifan nurul haq on 02/01/24.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemRed
+    }
+
+}
