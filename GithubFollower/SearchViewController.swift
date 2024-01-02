469a219ae2e383f8f56ca19d85fa116cc09a8b20
hanifan <hanifannurulhaq@outlook.com>
2024-01-02 19:25:46 +0700

    implement button component

--- GithubFollower/SearchViewController.swift
+++ GithubFollower/SearchViewController.swift
@@ -18,6 +18,7 @@ class SearchViewController: UIViewController {
         view.backgroundColor = .systemBackground
         configureLogoImageView()
         configureTextField()
+        configureCallToActionButton()
         
     }
     
@@ -48,6 +49,16 @@ class SearchViewController: UIViewController {
             usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
             usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
             usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    func configureCallToActionButton() {
+        view.addSubview(callToActionButton)
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 
