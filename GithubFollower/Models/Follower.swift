afeb814acaff3c1c2abd65d1979b02677acbd4bf
hanifan <hanifannurulhaq@outlook.com>
2024-01-23 20:34:00 +0700

    fix navbar is missing when navigation is aborted

---
+++ GithubFollower/Models/Follower.swift
@@ -0,0 +1,8 @@
+//
+//  Follower.swift
+//  GithubFollower
+//
+//  Created by hanifan nurul haq on 23/01/24.
+//
+
+import Foundation
---
+++ GithubFollower/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  GithubFollower
+//
+//  Created by hanifan nurul haq on 23/01/24.
+//
+
+import Foundation
