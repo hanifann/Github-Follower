b5727e6cff6c2f2c6fc5a639d48a2394d39d1509
hanifan <hanifannurulhaq@outlook.com>
2024-01-23 15:56:32 +0700

    add containerView for alert

--- GithubFollower/GFContainerView.swift
+++ GithubFollower/GFContainerView.swift
@@ -9,12 +9,26 @@ import UIKit
 
 class GFContainerView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        configure()
+    }
+    
+    private func configure() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
 }
