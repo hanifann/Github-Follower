d1083b199fa1a12701a2640d0d61ccdc0ee899bb
hanifan <hanifannurulhaq@outlook.com>
2024-01-23 21:04:22 +0700

    implement networkManager to test the result

--- GithubFollower/Screens/FollowerViewController.swift
+++ GithubFollower/Screens/FollowerViewController.swift
@@ -16,6 +16,16 @@ class FollowerViewController: UIViewController {
         
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollower(for: username, page: 1) { followers, errorMessage in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happen", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+            
+            print("Followers.count = \(followers.count)")
+            print(followers)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
