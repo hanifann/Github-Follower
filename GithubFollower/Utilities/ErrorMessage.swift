bdda60bbcfa257dd166c5a622c074a81e2cb3667
hanifan <hanifannurulhaq@outlook.com>
2024-01-23 21:14:01 +0700

    add errorMessage enum

---
+++ GithubFollower/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  GithubFollower
+//
+//  Created by hanifan nurul haq on 23/01/24.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUsername = "This username created and invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid resposne from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
