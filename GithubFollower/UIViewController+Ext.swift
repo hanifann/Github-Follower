5a6fe33d67ce6a099425f52f0f34df58869318ac
hanifan <hanifannurulhaq@outlook.com>
2024-01-23 15:57:15 +0700

    add extension on UIViewController to display custom alert

--- GithubFollower/UIViewController+Ext.swift
+++ GithubFollower/UIViewController+Ext.swift
@@ -5,4 +5,16 @@
 //  Created by hanifan nurul haq on 23/01/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertViewController = GFAllertViewController(alertTitle: title, message: message, buttonTitle: buttonTitle)
+            alertViewController.modalPresentationStyle = .overFullScreen
+            alertViewController.modalTransitionStyle = .crossDissolve
+            self.present(alertViewController, animated: true)
+        }
+    }
+}
