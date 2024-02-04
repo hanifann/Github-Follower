b664d4c346dd77e75ef17d4e1cee73d55c9f3512
hanifan <hanifannurulhaq@outlook.com>
2024-02-04 16:18:41 +0700

    add GFAvatarImageView component

--- GithubFollower/Custom Views/Cells/FollowersCell.swift
+++ GithubFollower/Custom Views/Cells/FollowersCell.swift
@@ -8,5 +8,45 @@
 import UIKit
 
 class FollowersCell: UICollectionViewCell {
+    static let reuseId = "FollowersCell"
+    
+    let avatarImage = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+    
+    private func configure() {
+        addSubview(avatarImage)
+        addSubview(usernameLabel)
+        
+        let padding: CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            avatarImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImage.heightAnchor.constraint(equalTo: avatarImage.widthAnchor),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImage.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+    
+    
+    
     
 }
