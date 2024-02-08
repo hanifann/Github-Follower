676e417f9742726752b75f86f6743e3bdc91b23c
hanifan <hanifannurulhaq@outlook.com>
2024-02-08 16:48:59 +0700

    display userinfo modal when item is clicked

--- GithubFollower/Screens/FollowerViewController.swift
+++ GithubFollower/Screens/FollowerViewController.swift
@@ -18,6 +18,7 @@ class FollowerViewController: UIViewController {
     var filteredFollowers: [Follower] = []
     var page = 1
     var hasMoreFollowers = true
+    var isSearching = false
     
     
     var collectionView: UICollectionView!
@@ -120,17 +121,29 @@ extension FollowerViewController: UICollectionViewDelegate {
             getFollowers(username: username, page: page)
         }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let activeFollowers = isSearching ? filteredFollowers : followers
+        let follower = activeFollowers[indexPath.item]
+        
+        let destinationViewController = UserInfoViewController()
+        destinationViewController.username = follower.login
+        
+        let navController = UINavigationController(rootViewController: destinationViewController)
+        present(navController, animated: true)
+    }
 }
 
 extension FollowerViewController: UISearchResultsUpdating, UISearchBarDelegate {
     func updateSearchResults(for searchController: UISearchController) {
         guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
-        
+        isSearching = true
         filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
         updateData(on: filteredFollowers)
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
         updateData(on: followers)
     }
 }
