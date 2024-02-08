b7792a5dc750667922fe0c5370359b80a34c7fca
hanifan <hanifannurulhaq@outlook.com>
2024-02-08 21:16:21 +0700

    add child view controller for User Info Header

--- GithubFollower/Custom Views/ViewControllers/GFUserInfoHeaderViewController.swift
+++ GithubFollower/Custom Views/ViewControllers/GFUserInfoHeaderViewController.swift
@@ -8,22 +8,88 @@
 import UIKit
 
 class GFUserInfoHeaderViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    var avatarImage = GFAvatarImageView(frame: .zero)
+    var usernameLabel = GFTitleLabel(textAlignment: .left, fontSize: 36)
+    var nameLabel = GFSecondaryTitleLabel(fontSize: 18)
+    var locationIconImageView = UIImageView()
+    var locationLabel = GFSecondaryTitleLabel(fontSize: 18)
+    var bioLabel = GFBodyLabel(textAlignment: .left)
+    
+    var user: User!
+    
+    init(user: User) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubView()
+        layoutUI()
+        configureUI()
+    }
+    
+    func configureUI() {
+        avatarImage.downloadImage(from: user.avatarUrl)
+        usernameLabel.text = user.login
+        nameLabel.text = user.name ?? ""
+        locationIconImageView.image = UIImage(systemName: SFSymbols.location)
+        locationIconImageView.tintColor = .secondaryLabel
+        locationLabel.text = user.location ?? "Location not available"
+        bioLabel.text = user.bio ?? "Bio not available"
+        bioLabel.numberOfLines = 3
+    }
+    
+    func addSubView() {
+        view.addSubview(avatarImage)
+        view.addSubview(usernameLabel)
+        view.addSubview(nameLabel)
+        view.addSubview(locationIconImageView)
+        view.addSubview(locationLabel)
+        view.addSubview(bioLabel)
+    }
+    
+    func layoutUI() {
+        let imagePadding: CGFloat = 12
+        let padding: CGFloat = 20
+        locationIconImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            avatarImage.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            avatarImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            avatarImage.widthAnchor.constraint(equalToConstant: 100),
+            avatarImage.heightAnchor.constraint(equalToConstant: 100),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImage.topAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImage.trailingAnchor, constant: imagePadding),
+            usernameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 38),
+            
+            nameLabel.centerYAnchor.constraint(equalTo: avatarImage.centerYAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: avatarImage.trailingAnchor, constant: imagePadding),
+            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            nameLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            locationIconImageView.bottomAnchor.constraint(equalTo: avatarImage.bottomAnchor),
+            locationIconImageView.leadingAnchor.constraint(equalTo: avatarImage.trailingAnchor, constant: imagePadding),
+            locationIconImageView.widthAnchor.constraint(equalToConstant: 20),
+            locationIconImageView.heightAnchor.constraint(equalToConstant: 20),
+            
+            locationLabel.centerYAnchor.constraint(equalTo: locationIconImageView.centerYAnchor),
+            locationLabel.leadingAnchor.constraint(equalTo: locationIconImageView.trailingAnchor, constant: 8),
+            locationLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            locationLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            bioLabel.topAnchor.constraint(equalTo: avatarImage.bottomAnchor, constant: padding),
+            bioLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            bioLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            bioLabel.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
 
 }
