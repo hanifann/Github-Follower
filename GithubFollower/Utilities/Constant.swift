83d5d7a1f106cd6a21c4ed052a6f92fc8c49d423
hanifan <hanifannurulhaq@outlook.com>
2024-02-08 21:15:34 +0700

    add enum for SFSymbols name

---
+++ GithubFollower/Utilities/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  GithubFollower
+//
+//  Created by hanifan nurul haq on 08/02/24.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
