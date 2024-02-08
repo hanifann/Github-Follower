45404056e604d3c6f385f9c6a7df5c44937fb354
hanifan <hanifannurulhaq@outlook.com>
2024-02-08 19:35:01 +0700

    change name

--- GithubFollower/Utilities/GFError.swift
+++ GithubFollower/Utilities/GFError.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+enum GFError: String, Error {
+    case invalidUsername = "This username created and invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid resposne from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
