86294b8c5f7ee250b416108417f0749865b57656
hanifan <hanifannurulhaq@outlook.com>
2024-02-11 14:29:52 +0700

    add Repo Item and Follower Item View Controllers

--- GithubFollower/Custom Views/ViewControllers/GFFollowerItemViewController.swift
+++ GithubFollower/Custom Views/ViewControllers/GFFollowerItemViewController.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class GFFollowerItemViewController: GFItemInfoViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionbutton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+
+}
